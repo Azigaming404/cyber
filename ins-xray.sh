9f580b1cb3847d8e16f7ed0c33c8070bead6e5ea
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-06-27 20:03:44 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -129,13 +129,13 @@ install_ssl(){
 mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/vps.conf.txt"
 sleep 1
-wget -q -O xraymode.sh http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
+wget -q -O xraymode.sh https://raw.githubusercontent.com/Azigaming404/cyber/main/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
 sleep 1 
-wget -q -O /etc/xray/config.json "http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/configuration/config.json"
+wget -q -O /etc/xray/config.json "https://raw.githubusercontent.com/Azigaming404/cyber/main/configuration/config.json"
 chmod +x /etc/xray/config.json
 sleep 1 
 rm -f /etc/nginx/conf.d/xray.conf
-wget -q -O /etc/nginx/conf.d/xray.conf "http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/configuration/xray.conf"
+wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/Azigaming404/cyber/main/xray.conf"
 chmod +x /etc/nginx/conf.d/xray.conf
 
 # Installing Xray Service
